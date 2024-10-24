3523755c302e6fa165d767cddfec0d135e391607
Kamlesh Bhatt <kamleshbhatt3103@gmail.com>
2024-10-24 22:45:35 +0530

    feat: created a seprate loadingModal that can be used anywhere

---
+++ src/components/LoadingModal.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Modal,
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+} from "react-native";
+
+const LoadingModal = ({ visible, onClose, title }) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalView}>
+          <ActivityIndicator size="large" color="#79d2eb" />
+          <Text style={styles.loadingText}>{title}</Text>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    backgroundColor: "#fff",
+    width: "80%",
+    borderRadius: 12,
+    padding: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  loadingText: {
+    marginTop: 10,
+    fontSize: 16,
+    color: "#79d2eb",
+  },
+});
+
+export default LoadingModal;
