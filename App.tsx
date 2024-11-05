5ad83c0e82b9888db14549a66d7318099a1e9269
Kamlesh Bhatt <kamleshbhatt3103@gmail.com>
2024-11-05 23:32:09 +0530

    fix: App.tsx structure

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./src/screens/auth/Login";
-import ContextProvider from "./src/contexts/ContextProvider";
 
-import Home from "./src/screens/Home";
+// Import screens
+import Login from "./src/screens/auth/Login";
 import SignUp from "./src/screens/auth/Signup";
+import Home from "./src/screens/Home";
 import Profile from "./src/screens/Profile/Profile";
 
+// Import context provider
+import ContextProvider from "./src/contexts/ContextProvider";
+
+// Create stack navigator
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <ContextProvider>
+        <StatusBar style="auto" />
         <Stack.Navigator
           initialRouteName="Login"
           screenOptions={{ headerShown: false }}
